7284c178f056dd9cc3af1cad5f811cfbf7c61003
suchi-291 <suryasuchitha@gmail.com>
2026-05-25 21:53:05 +0530

    initial Springboot project setup

---
+++ src/main/java/com/restro/RestroOnboardingBeApplication.java
@@ -0,0 +1,13 @@
+package com.restro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestroOnboardingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestroOnboardingBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restro/RestroOnboardingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestroOnboardingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
